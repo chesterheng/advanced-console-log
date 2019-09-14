56e43cfd3f6e441bc41608580629ce4a073b7550
Chester Heng <chesterheng@Chesters-MacBook-Pro.local>
2019-09-14 13:06:16 +0800

    Advanced Console Log Arguments

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ console.warn("this is a warning!");
 console.error("this is an error!");
 console.info("this is info!");
 console.debug("this is debug!");
+
+console.log("hello work my name is %s and I am %d years old and here is object %o", "Chester", 33, { x: "foo"});
+console.log("%coutput", "color: blue; font-size: 33px");
