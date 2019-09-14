79a5b30b6f28f8512dee34fe442f3ea5f7dea485
Chester Heng <chesterheng@Chesters-MacBook-Pro.local>
2019-09-14 13:36:36 +0800

    Logging Pretty-Printing Tabular Data to the Console

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ console.error("this is an error!");
 console.info("this is info!");
 console.debug("this is debug!");
 
-console.log("hello work my name is %s and I am %d years old and here is object %o", "Chester", 33, { x: "foo"});
+console.log("hello work my name is %s and I am %d years old and here is object %o", "Chester", 33, {
+  x: "foo"
+});
 console.log("%coutput", "color: blue; font-size: 33px");
 
-for(let i = 0; i < 100; i++) {
+for (let i = 0; i < 100; i++) {
   let num = Math.random() * 100;
   console.groupCollapsed("Pick a random numbers");
   console.log("Number > 10", num > 10);
@@ -19,15 +21,15 @@ for(let i = 0; i < 100; i++) {
 }
 
 const foo = undefined;
-if(!foo) {
+if (!foo) {
   console.error("foo wasn't set");
 }
 console.assert(foo, "foo wasn't set");
 
-for(let i = 0; i < 100; i++) {
+for (let i = 0; i < 100; i++) {
   let num = Math.random() * 100;
 
-  if(num > 50) {
+  if (num > 50) {
     console.count("greater than 50");
   } else {
     console.count("less than 50");
@@ -36,7 +38,28 @@ for(let i = 0; i < 100; i++) {
 
 console.time("createObject");
 const array = [];
-for(let i = 0; i < 10000; i++) {
-  array.push({ index: i });
+for (let i = 0; i < 10000; i++) {
+  array.push({
+    index: i
+  });
 }
 console.timeEnd("createObject");
+
+const Character = (name, power) => ({
+  name,
+  power
+});
+
+const buffy = Character("buffy", "slayer");
+const willow = Character("wollow", "witch");
+const spike = Character("spike", "vampire");
+
+const chars = [buffy, willow, spike];
+
+const charsByName = {
+  buffy,
+  willow,
+  spike
+};
+
+console.table(charsByName, ["name", "power"]);
