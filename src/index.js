fc63ebd04ed92f64755bcfd801a15d74e6abb7f6
Chester Heng <chesterheng@Chesters-MacBook-Pro.local>
2019-09-14 13:12:03 +0800

    Grouping and Nesting Console Log Output

--- src/index.js
+++ src/index.js
@@ -6,3 +6,14 @@ console.debug("this is debug!");
 
 console.log("hello work my name is %s and I am %d years old and here is object %o", "Chester", 33, { x: "foo"});
 console.log("%coutput", "color: blue; font-size: 33px");
+
+for(let i = 0; i < 100; i++) {
+  let num = Math.random() * 100;
+  console.groupCollapsed("Pick a random numbers");
+  console.log("Number > 10", num > 10);
+  console.log("Number > 50", num > 50);
+  console.groupCollapsed("This is another nested log group");
+  console.log();
+  console.groupEnd();
+  console.groupEnd();
+}
